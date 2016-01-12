db9486cb43aa9042231f0a784777e7b1da10e0a7
Neil Gabbadon <neil.gabbadon@emikra.com>
2016-01-12 16:36:56 -0500

    Makes style-propable mixin solely dependent on styleUtils
    This commit fixes is a regression introduced in commit 2592cdafedd52a60e82144dce6ffb83675ce96a1 in which `mergeAndPrefix()` was accidentally removed from the `styleUtils`.  This commit adds the method back, and it also makes `style-propable` only use methods from the `styleUtils` module rather than from both `styleUtils` and `immutability-helper`.

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,20 +1,22 @@
 import AutoPrefix from '../styles/auto-prefix';
-import ImmutabilityHelper from '../utils/immutability-helper';
+import {merge} from '../utils/immutability-helper';
 import warning from 'warning';
 
 const reTranslate = /((^|\s)translate(3d|X)?\()(\-?[\d]+)/;
 
 const reSkew = /((^|\s)skew(x|y)?\()\s*(\-?[\d]+)(deg|rad|grad)(,\s*(\-?[\d]+)(deg|rad|grad))?/;
 
-
-// This function ensures that `style` supports both ltr and rtl directions by checking
-//   `styleConstants` in `muiTheme` and replacing attribute keys if necessary.
+/**
+ * This function ensures that `style` supports both ltr and rtl directions by
+ * checking `styleConstants` in `muiTheme` and replacing attribute keys if
+ * necessary.
+ */
 function ensureDirection(muiTheme, style) {
   if (process.env.NODE_ENV !== 'production') {
     warning(!style.didFlip, `You're calling ensureDirection() on the same style
       object twice.`);
 
-    style = ImmutabilityHelper.merge({
+    style = merge({
       didFlip: 'true',
     }, style);
   }
@@ -91,12 +93,37 @@ function ensureDirection(muiTheme, style) {
 
 export default {
 
-  merge() {
-    return ImmutabilityHelper.merge.apply(this, arguments);
+  /**
+   * `merge` is used to merge styles together.
+   *
+   * This method currently used the merge implementation from
+   * `utils/immutability-helper.js`
+   */
+  merge,
+
+  /**
+   * `mergeAndPrefix` is used to merge styles and autoprefix them.
+   *
+   * It has has been deprecated and should no longer be used.
+   */
+  mergeAndPrefix(...args) {
+    warning(false, 'Use of mergeAndPrefix() has been deprecated. ' +
+      'Please use mergeStyles() for merging styles, and then prepareStyles() for prefixing and ensuring direction.');
+    return AutoPrefix.all(merge(...args));
   },
 
+  /**
+   * `prepareStyles` is used to merge multiple styles, make sure they are flipped
+   * to rtl if needed, and then autoprefix them.
+   *
+   * Never call this on the same style object twice. As a rule of thumb, only
+   * call it when passing style attribute to html elements.
+   *
+   * If this method detects you called it twice on the same style object, it
+   * will produce a warning in the console.
+   */
   prepareStyles(muiTheme, ...styles) {
-    styles = styles.length > 1 ? ImmutabilityHelper.merge.apply(this, styles) : (styles[0] || {});
+    styles = styles.length > 1 ? merge(...styles) : (styles[0] || {});
     const flipped = ensureDirection(muiTheme, styles);
     return AutoPrefix.all(flipped);
   },
