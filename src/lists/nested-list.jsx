0fd8308733443a9a89f15d1f0bcc4635f13689ed
Neil Gabbadon <neil.gabbadon@emikra.com>
2016-01-12 16:38:12 -0500

    Replaces ImmutabilityHelper with styleUtils for style merging

--- src/lists/nested-list.jsx
+++ src/lists/nested-list.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ImmutabilityHelper from '../utils/immutability-helper';
+import styleUtils from '../utils/styles';
 import List from './list';
 
 
@@ -39,7 +39,7 @@ const NestedList = React.createClass({
     };
 
     return (
-      <List style={ImmutabilityHelper.merge(styles.root, style)}>
+      <List style={styleUtils.merge(styles.root, style)}>
         {
           React.Children.map(children, (child) => {
             return React.isValidElement(child) ? (
